ab3f8e38fdfc256ef5255c9eb5d4ea4e06bdfd21
Younex <youpc99@gmail.com>
2026-07-18 13:08:10 +0100

    fresh REACT.ts , tailwindCSS v4

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="m-auto mt-[10%] flex w-fit flex-col justify-center gap-10 rounded-3xl bg-black/20 p-10 text-2xl font-bold text-white">
+      <div>React</div>
+      <div>Typescript</div>
+      <div>Vite</div>
+      <div>TailwindCSS</div>
+    </div>
+  );
+}
+
+export default App;
