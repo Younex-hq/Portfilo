f9d8a86814299e6026d8505de08b13a2fe0ef2f3
Younex <youpc99@gmail.com>
2026-07-18 21:46:01 +0100

    add more interaction distance to TextPressure

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,9 +1,13 @@
 import NavElements from "../common/NavElements";
+import TextPressure from "../common/TextPressure";
 
 export default function Hero() {
   return (
-    <div className="flex h-screen flex-col items-center justify-around bg-red-900">
-      <h1>HELLO !</h1>
+    <div className="relative flex h-screen flex-col items-center justify-around">
+      {/*<h1>HELLO !</h1>*/}
+      <div className="absolute mt-[50vh] h-screen w-[50vw]">
+        <HelloText />
+      </div>
       <div>
         Welcome to my <span className="font-italianno text-4xl">Portfolio</span>
       </div>
@@ -11,5 +15,23 @@ export default function Hero() {
         <NavElements />
       </div>
     </div>
+  );
+}
+
+function HelloText() {
+  return (
+    <TextPressure
+      text="Hello!"
+      flex
+      alpha={false}
+      stroke={false}
+      width={true}
+      weight
+      italic
+      textColor="#ffffff"
+      strokeColor="#2856f5"
+      minFontSize={136}
+      maxDistance={650}
+    />
   );
 }
